0a662fbb08b376c6efeb45888f2f4fd9e6c2fec5
sanika2001 <porayilsanika@gmail.com>
2022-07-05 23:02:02 +0530

    Time change screen button

--- lib/Screens/TimeChangeScreen.dart
+++ lib/Screens/TimeChangeScreen.dart
@@ -44,63 +44,83 @@ class _TimeChangeScreenState extends State<TimeChangeScreen> {
             padding: const EdgeInsets.only(top: 18, left: 18),
             child: Column(
               children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 27),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Status',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12,
+                          fontFamily: 'SFUIText',
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: dropdownvalue == "Open"
+                                ? Colors.green
+                                : Colors.red,
+                            borderRadius:
+                                BorderRadius.circular(5.454545497894287),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 7),
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton(
+                                value: dropdownvalue,
+                                dropdownColor: const Color(0xff042e60),
+                                // Down Arrow Icon
+                                icon: const Icon(
+                                  Icons.keyboard_arrow_down,
+                                  color: Colors.white,
+                                ),
+
+                                // Array list of items
+                                items: items.map((String items) {
+                                  return DropdownMenuItem(
+                                    value: items,
+                                    child: Text(
+                                      items,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                        fontFamily: 'SFUIText',
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  );
+                                }).toList(),
+                                // After selecting the desired option,it will
+                                // change button value to selected value
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    dropdownvalue = newValue!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 Row(
-                  children: [
+                  children: const [
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.black,
+                      size: 12,
+                    ),
                     Text(
-                      'Status',
+                      'Set Timer',
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 12,
                         fontFamily: 'SFUIText',
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: dropdownvalue == "Open"
-                              ? Colors.green
-                              : Colors.red,
-                          borderRadius:
-                              BorderRadius.circular(5.454545497894287),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 7),
-                          child: DropdownButtonHideUnderline(
-                            child: DropdownButton(
-                              value: dropdownvalue,
-                              dropdownColor: const Color(0xff042e60),
-                              // Down Arrow Icon
-                              icon: const Icon(
-                                Icons.keyboard_arrow_down,
-                                color: Colors.white,
-                              ),
-
-                              // Array list of items
-                              items: items.map((String items) {
-                                return DropdownMenuItem(
-                                  value: items,
-                                  child: Text(
-                                    items,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 14,
-                                      fontFamily: 'SFUIText',
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                );
-                              }).toList(),
-                              // After selecting the desired option,it will
-                              // change button value to selected value
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  dropdownvalue = newValue!;
-                                });
-                              },
-                            ),
-                          ),
-                        ),
                       ),
                     ),
                   ],
