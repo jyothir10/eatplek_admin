ea9e0d08f1a2d219731080261393d7f60e50f9d2
sanika2001 <porayilsanika@gmail.com>
2022-12-28 02:08:50 +0530

    profile screen completed

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
     ]);
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: ProfileScreen.id,
+        initialRoute: OnboardingScreen.id,
         routes: {
           OnboardingScreen.id: (context) => OnboardingScreen(),
           LoginScreen.id: (context) => LoginScreen(),
