8105777980582653b1eea8a8bc43914d1a489016
sanika2001 <porayilsanika@gmail.com>
2023-01-09 23:51:07 +0530

    minor changes

--- lib/Screens/RevenueScreen.dart
+++ lib/Screens/RevenueScreen.dart
@@ -132,6 +132,9 @@ class _RevenueScreenState extends State<RevenueScreen> {
           },
           child: ModalProgressHUD(
             inAsyncCall: showSpinner,
+            progressIndicator: CircularProgressIndicator(
+              color: primaryClr,
+            ),
             child: Column(
               children: [
                 Container(
